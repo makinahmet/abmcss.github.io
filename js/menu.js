b5803adb002b353ddb114b0ea98133c27a32d9cd
makinahmet <makinahmet@gmail.com>
2024-02-23 18:18:13 +0300

    update

--- js/menu.js
+++ js/menu.js
@@ -54,6 +54,13 @@ Navbars
 Forms
 </a>
 <a class="text-black" href="icons.html">Icons</a>
+<a class="text-black" href="collapse.html">
+    <svg fill="#000000" width="16" height="16" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19 9h-2.58l3.29-3.29a1 1 0 1 0-1.42-1.42L15 7.57V5a1 1 0 0 0-1-1 1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 0-2z"/>
+        <path d="M10 13H5a1 1 0 0 0 0 2h2.57l-3.28 3.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L9 16.42V19a1 1 0 0 0 1 1 1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1z"/>
+    </svg>
+Collapsibles
+</a>
 <hr>`;
 
 var leftMenu = document.getElementById("leftMenu");
